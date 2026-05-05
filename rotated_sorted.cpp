e2bc815e2fdd3e31c94b2d6a7d6198efb9947f06
Mohamed ELGendy <mm6920@o365.fayoum.edu.eg>
2026-05-05 17:39:23 +0300

    Refactor rotated_sorted function and improve I/O
    Refactor rotated_sorted function for clarity and efficiency. Update input prompts and output messages.

--- rotated_sorted.cpp
+++ rotated_sorted.cpp
@@ -1,64 +1,57 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 int comparisons = 0;
 
-bool rotated_sorted(int arr[],int target , int n) {
-    int l = 0, r =  n - 1;
+// Best Case:  O(1)
+// Average Case: O(log n)
+// Worst Case: O(n)
+
+bool rotated_sorted(int arr[], int target, int n) {
+    int l = 0, r = n - 1;
     while (l <= r) {
-
         int mid = l + (r - l) / 2;
-
         comparisons++;
         if (arr[mid] == target) {
             return true;
         }
-
         if (arr[l] == arr[mid] && arr[mid] == arr[r]) {
             l++;
             r--;
-        }
-
-        if (arr[l]<=arr[mid]) {
-            comparisons +=2;
-            if (arr[l]<=target && target < arr[mid]) {
+        } else if (arr[l] <= arr[mid]) {
+            comparisons += 2;
+            if (arr[l] <= target && target < arr[mid]) {
                 r = mid - 1;
-            }
-            else {
+            } else {
                 l = mid + 1;
             }
-        }
-
-        else {
-            comparisons +=2;
-            if (arr[mid]<=target && target < arr[r]) {
+        } else {
+            comparisons += 2;
+            if (arr[mid] <= target && target < arr[r]) {
                 l = mid + 1;
-            }
-            else {
+            } else {
                 r = mid - 1;
             }
         }
     }
     return false;
 }
+
 int main() {
-    int n,target;
-    cout<<"please input the size of array: ";
-    cin>>n;
+    int n, target;
+    cout << "Please input the size of array: ";
+    cin >> n;
     int arr[n];
-    cout<<"please input the elements of array: ";
-    for(int i = 0;i < n;i++) {
-        cin>>arr[i];
-    }
-    cout<<"please input the target value: ";
-    cin>>target;
+    cout << "Please input the elements of array: ";
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    cout << "Please input the target value: ";
+    cin >> target;
+
     if (rotated_sorted(arr, target, n)) {
-        cout << "found" << endl;
-        cout<<"number of comparisons: "<< comparisons <<endl;
+        cout << "Found" << endl;
+    } else {
+        cout << "Not found" << endl;
     }
-    else {
-        cout << "not found:" << endl;
-    }
+    cout << "Number of comparisons: " << comparisons << endl;
 }
